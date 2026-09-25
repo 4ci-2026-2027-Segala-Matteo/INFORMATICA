31f880775ab2857d033ab77fb3c6b50f0e6adaed
matteosegala <segalamatteo09@gmail.com>
2026-09-25 19:42:01 +0200

    Create Animale.java

---
+++ Dogsitter/Animale.java
@@ -0,0 +1,93 @@
+public class Animale {
+    private String nome;
+    private String specie;
+    private int eta;
+    private int energia;
+
+    public Animale(){
+        this.nome = null;
+        this.specie = null;
+        this.eta = 0;
+        this.energia = 0;
+    }
+    
+    public Animale(String n, String s, int anni, int ene) {
+        if(n != null){
+            this.nome = n;
+        }
+        if(s != null){
+            this.specie = s;
+        }
+        if(anni >= 0){
+            this.eta = anni;
+        }
+        if(ene >= 0 && ene <= 100){
+            this.energia = ene;
+        }
+    }
+
+    public void setNome(String n) {
+        if(n != null){
+            this.nome = n;
+        }
+    }
+
+    public void setEta(int anni) {
+        if(anni >= 0){
+            this.eta = anni;
+        }
+    }
+
+    public void setSpecie(String s) {
+        if(s != null){
+            this.specie = s;
+        }
+    }
+
+    public void setEnergia(int ene) {
+        if(ene >= 0 && ene <= 100){
+            this.energia = ene;
+        }
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getEta() {
+        return this.eta;
+    }
+
+    public String getSpecie() {
+        return this.specie;
+    }
+
+    public int getEnergia() {
+        return this.energia;
+    }
+
+    public void gioca(int quantita) {
+        if(this.energia - quantita >= 5){
+            this.energia -= quantita;
+        } else {
+            this.energia = 0;
+        }
+    }
+
+    public void mangia(int quantità){
+        if(this.energia + quantità <= 100){
+            this.energia += quantità;
+        } else {
+            this.energia = 100;
+        }
+    }
+
+    public String toString() {
+        String out;
+        out = "Il nome dell'animale è: " + this.nome;
+        out += "\nLa specie dell'animale è: " + this.specie;
+        out += "\nL'età dell'animale è: " + this.eta;
+        out += "\nL'energia dell'animale è: " + this.energia;
+        return out;
+    }
+}
